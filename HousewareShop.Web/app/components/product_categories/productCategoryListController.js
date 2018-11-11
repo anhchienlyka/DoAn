e103c48cef083a6d897a93fab0e5bfb7276eee87
anhchienlyka <phamchien96nd@gmail.com>
2018-11-11 11:16:36 +0700

    bai27

--- HousewareShop.Web/app/components/product_categories/productCategoryListController.js
+++ HousewareShop.Web/app/components/product_categories/productCategoryListController.js
@@ -1,15 +1,17 @@
 ﻿(function (app) {
     app.controller('productCategoryListController', productCategoryListController);
 
-    productCategoryListController.$inject = ['$scope', 'apiService'];
+    productCategoryListController.$inject = ['$scope', 'apiService', 'notificationService'];
 
-    function productCategoryListController($scope, apiService) {
+    function productCategoryListController($scope, apiService, notificationService) {
         $scope.productCategories = [];
         $scope.page = 0;
         $scope.pagesCount = 0;
         $scope.getProductCagories = getProductCagories;
         $scope.keyword = '';
-        $scope.search = search;// sự kiện tìm kiếm
+
+        $scope.search = search;
+
         function search() {
             getProductCagories();
         }
@@ -23,6 +25,14 @@
                 }
             }
             apiService.get('/api/productcategory/getall', config, function (result) {
+                if (result.data.TotalCount == 0) {
+                    notificationService.displayWarning('Không có bản ghi nào được tìm thấy.');
+                    
+                }
+          
+                else {
+                    notificationService.displaySuccess('Đã tìm thấy ' + result.data.TotalCount + ' bản ghi.');
+                }
                 $scope.productCategories = result.data.Items;
                 $scope.page = result.data.Page;
                 $scope.pagesCount = result.data.TotalPages;
